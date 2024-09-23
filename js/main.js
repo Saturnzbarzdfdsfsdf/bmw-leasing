1c215ff7bbd8ecb790b3f157c50559264c9ca8e8
Timur <saturnianin777@gmail.com>
2024-09-23 16:17:32 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper', {
+	// Optional parameters
+	direction: 'horizontal',
+	loop: true,
+  effect: "fade",
+	pagination: {
+		el: '.swiper-pagination',
+	},
+	// autoplay: {
+	// 	delay: 3000,
+	// 	pauseOnMouseEnter: true,
+	// },
+})
+
